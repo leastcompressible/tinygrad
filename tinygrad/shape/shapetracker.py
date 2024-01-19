a470e8d2629b1477e7cf8fae241a4c66e047f493
chenyu <chenyu@fastmail.com>
2024-01-19 15:13:49 -0800

    does anything fail?

--- tinygrad/shape/shapetracker.py
+++ tinygrad/shape/shapetracker.py
@@ -9,8 +9,8 @@ from tinygrad.shape.view import View
 
 @functools.lru_cache(maxsize=None)
 def merge_views(vm2:View, vm1:View) -> Optional[View]:
-  if vm1.contiguous and vm1.shape == vm2.shape: return vm2
-  if vm2.contiguous: return vm1
+  # if vm1.contiguous and vm1.shape == vm2.shape: return vm2
+  # if vm2.contiguous: return vm1
   if vm2.mask or vm1.offset != 0: return None  # this isn't supported yet
   if None in (strides := ShapeTracker((vm2, vm1)).real_strides()): return None
   return View.create(vm1.shape, cast(Tuple[sint, ...], strides), vm2.offset, vm1.mask)
