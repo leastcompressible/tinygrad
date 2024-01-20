fix overflow issue of Tensor.maximum
(self/2 + x/2) instead of (self+x)/2. also enabled lt and eq tests in test_dtype_alu

test/test_dtype_alu.py
@@ -26,11 +26,9 @@ unary_operations = [(Tensor.exp, np.exp), (Tensor.log, np.log), operator.neg, (T
 # TODO: enable mod on Tensor
 #binary_operations.append(operator.mod)
 
-# TODO: lt and eq should cast in tensor before we can test them, this is a separate project
-#binary_operations += [operator.lt, operator.eq]
+binary_operations += [operator.lt, operator.eq]
 
-# TODO: (a+b)/2 in tensor.py's maximum can overflow. This requires a new implementation of maximum that can be backpropagated
-#binary_operations += [(Tensor.maximum, np.maximum)]
+binary_operations += [(Tensor.maximum, np.maximum)]
 
 # TODO: CUDACPU segfaults on sin
 if getenv("CUDACPU"): unary_operations.remove((Tensor.sin, np.sin))