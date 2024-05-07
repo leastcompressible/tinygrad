fdc3bbfa1fec21e3f997478f028bf706a10fed70
chenyu <chenyu@fastmail.com>
2024-05-07 03:06:31 +0000

    not these

--- tinygrad/lazy.py
+++ tinygrad/lazy.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 import math
 from typing import Union, Optional, Any, Tuple, List
-from tinygrad.dtype import dtypes, DType, ConstType, least_upper_dtype
+from tinygrad.dtype import dtypes, DType, ConstType, least_upper_dtype, sum_acc_dtype
 from tinygrad.helpers import prod, getenv, all_int, all_same, DEBUG
 from tinygrad.ops import LoadOps, UnaryOps, BinaryOps, TernaryOps, ReduceOps, Op, exec_alu, python_alu
 from tinygrad.shape.symbolic import sint, Variable
@@ -174,15 +174,12 @@ class LazyBuffer:
     if self.size == 0 and 0 not in new_shape: return self.const({ReduceOps.SUM: 0.0, ReduceOps.MAX: -math.inf}[op], new_shape)
 
     # upcast acc_dt here so if reduce is splitted, the intermediate dtype is upcasted
-    if op is ReduceOps.SUM and acc_dt is None:
-      acc_dt = least_upper_dtype(self.dtype, dtypes.uint) if dtypes.is_unsigned(self.dtype) else \
-               least_upper_dtype(self.dtype, dtypes.int) if (dtypes.is_int(self.dtype) or self.dtype==dtypes.bool) else \
-               least_upper_dtype(self.dtype, dtypes.float)
+    if op is ReduceOps.SUM and acc_dt is None: acc_dt = sum_acc_dtype(self.dtype)
     if acc_dt is not None and acc_dt != self.dtype:
       # cast back to float16 or bfloat16 to match torch / jax behavior
       return self.cast(acc_dt).r(op, axis, acc_dt).cast(self.dtype if self.dtype in [dtypes.float16, dtypes.bfloat16] else acc_dt)
 
-    # const folding after acc_dt cast to correct output dtype
+    # const folding
     if self.is_unrealized_unmasked_const():
       return self.const(self.base.arg * {ReduceOps.SUM: prod(self.shape[i] for i in axis), ReduceOps.MAX: 1}[op], new_shape)
 
