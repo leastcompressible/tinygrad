bf4a790da73b405abed2c28328f1687e3699cafd
Chen-Yu Yang <chenyu@fastmail.com>
2024-12-01 20:56:09 -0500

    test ceildiv

--- tinygrad/helpers.py
+++ tinygrad/helpers.py
@@ -43,6 +43,7 @@ def fromimport(mod, frm): return getattr(__import__(mod, fromlist=[frm]), frm)
 def strip_parens(fst:str): return fst[1:-1] if fst[0] == '(' and fst[-1] == ')' and fst[1:-1].find('(') <= fst[1:-1].find(')') else fst
 def ceildiv(num, amt):
   ret = -(num//-amt)
+  if isinstance(ret, float): raise
   return ret if not isinstance(ret, float) else int(ret)
 def round_up(num:int, amt:int) -> int: return (num+amt-1)//amt * amt
 def data64(data:Any) -> Tuple[Any, Any]: return (data >> 32, data & 0xFFFFFFFF) # Any is sint
